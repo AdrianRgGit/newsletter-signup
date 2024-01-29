507c0145e04998155f27a63d1c0a5bb8c6a69a03
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-01-29 21:06:20 +0100

    first conf

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "red-1": "hsl(4, 100%, 67%)",
+        "gray-1": "hsl(235, 18%, 26%)",
+        "gray-2": "hsl(231, 7%, 60%)",
+        "gray-3": "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
