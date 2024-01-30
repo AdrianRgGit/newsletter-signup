94ba1ec1d52f9e2b1618ca1fb37e274b023e3036
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-01-30 11:42:49 +0100

    hecho el form

---
+++ src/components/Form.jsx
@@ -0,0 +1,57 @@
+import { useState } from "preact/hooks";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [invalid, setInvalid] = useState(false);
+
+  const reExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (reExp.test(email)) {
+      setInvalid(false);
+      console.log("Válido");
+    } else {
+      setInvalid(true);
+      console.log("Inválido");
+
+      setTimeout(() => {
+        setInvalid(false);
+      }, 3000);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  };
+
+  return (
+    <form onSubmit={onSubmitForm} class="flex flex-col gap-y-4">
+      <label class="flex flex-col text-gray-1 font-semibold text-sm">
+        <div class="flex justify-between items-center">
+          <p>Email address</p>
+          {invalid ? <p class="text-red-1">Valid email required</p> : null}
+        </div>
+        <input
+          type="text"
+          name="email"
+          placeholder="email@company.com"
+          class={`p-4 rounded-lg border mt-2 outline-none ${
+            invalid ? "bg-red-1/20 border-red-1 text-red-1" : "border-gray-2"
+          }`}
+          value={email}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button
+        class="bg-gray-1 text-white p-4 rounded-lg font-bold"
+        type="submit"
+      >
+        Subscribe to monthly newsletter
+      </button>
+    </form>
+  );
+};
+
+export default Form;
